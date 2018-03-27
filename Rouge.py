93d96d09d05212ab0bc7a1add94a2e0767c731d7
arranzignacio <arranzignacio@gmail.com>
2018-03-27 14:41:01 -0400

    minor edits

--- Rouge.py
+++ Rouge.py
@@ -4,10 +4,20 @@ def rouge_metrics(lst1, lst2):
 	rouge_precision = len(intersection(system_list,reference_list))/system_word_count
 
 
-
 def intersection(system_lst, ref_lst):
-    lst3 = [value for value in lst1 if value in lst2]
-    return lst3
+    intersection_lst = [value for value in system_lst if value in ref_lst]
+    return intersection_lst
+
+def create_ngrams(text_list,n=2):
+	iterations = len(text_list)-n
+	ngrams = []
+	gram = []
+	for i in range(iterations+1):
+		gram = text_list[i:n+i]
+		ngrams.append(gram)
+
+	return ngrams
+
 
 system_text = "the cat was found under the bed"
 reference_text = "the cat was under the bed"
@@ -23,9 +33,13 @@ reference_word_count = len(reference_list)
 rouge_recall = len(intersection(system_list,reference_list))/reference_word_count
 rouge_precision = len(intersection(system_list,reference_list))/system_word_count
 
+ngrams = create_ngrams(system_list,2)
 
+print(ngrams)
 print("recall",rouge_recall)
 print("precision",rouge_precision)
+
+#chris said that averaging this metric for every text was fine
 
 #chris said that averaging this metric for every text was fine
 
