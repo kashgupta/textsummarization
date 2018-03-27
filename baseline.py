ba9a188b34cda1756185452547013567f5fb1d26
arranzignacio <arranzignacio@gmail.com>
2018-03-27 15:34:17 -0400

    baseline done

--- baseline.py
+++ baseline.py
@@ -1,14 +1,17 @@
+import re
 
 def clean_data(line):
     line = line.split()
     line = [word.lower() for word in line]
-    line = [re.sub("[,.-!?]"," ",word) for word in line] # Remove punctuation in the text
+    line = [re.sub("[,-\.!?]"," ",word) for word in line] # Remove punctuation in the text
     #line = [re.sub("[\(][a-zA-z0-9]*[\)]"," ", word) for word in line] # Remove words within brackets 
     #line = [re.sub("[0-9]*[.]*[0-9]*"," ", word) for word in line]
     return " ".join(line)
 
+#\.-!?
 
-filename = "training_texts.txt"
+
+filename = "../X_data_train.txt"
 
 with open(filename,"r") as f:
 	data = f.read()
@@ -25,7 +28,7 @@ for line in lines:
 y_pred = [clean_data(summary) for summary in y_pred]
 
 
-with open("baseline.txt","w") as f:
+with open("../baseline.txt","w") as f:
 	for line in y_pred:
 		f.write(line)
 		f.write("\n")
